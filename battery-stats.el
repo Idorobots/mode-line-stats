fa46a12949c887f0805b2579dc9e0c1832e95c8d
Andreu Gil <agpchil@gmail.com>
2013-04-20 21:17:00 +0200

    Add battery-stats

---
+++ battery-stats.el
@@ -0,0 +1,147 @@
+;;; battery-stats.el --- display various disk stats in the mode-line  -*- coding: mule-utf-8 -*-
+
+;; This file is not part of Emacs
+
+;; Copyright (C) 2013 Andreu Gil Pàmies
+;; Copyright (C) 2012 Kajetan Rzepecki
+
+;; Author: Andreu Gil Pàmies <agpchil@gmail.com>
+
+;; Created: 05-04-2013
+
+;; Keywords: hardware
+
+;; This file is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
+
+;;; Usage:
+
+;; (require 'battery-stats)
+;; (setq battery-usage-format "%p")
+;; (battery-usage-start)
+
+;;; Commentary:
+
+;;; Code:
+
+(require 'cl)
+(require 'misc-utils)
+
+(defvar battery-usage-formatters nil)
+(defvar battery-usage-device nil)
+(defvar battery-usage-timer nil)
+(defvar battery-usage-mode-line-string "")
+(defvar battery-usage-use-global-mode-string t)
+
+(defvar battery-usage-battery-format "%c %r %B %d %L %p %m %h %t")
+
+(defgroup battery-usage nil
+  "Display various disk stats in the mode-line."
+  :group 'battery-usage)
+
+(defcustom battery-usage-update-interval 15
+  "Number of seconds between disk stats recalculation."
+  :type 'number
+  :group 'battery-usage)
+
+
+(defcustom battery-usage-format "%p %t %r"
+  "Format string:
+%c Current capacity (mAh or mWh)
+%r Current rate of charge or discharge
+%B Battery status (verbose)
+%b Battery status: empty means high, `-' means low,
+   `!' means critical, and `+' means charging
+%d Temperature (in degrees Celsius)
+%L AC line status (verbose)
+%p Battery load percentage
+%m Remaining time (to charge or discharge) in minutes
+%h Remaining time (to charge or discharge) in hours
+%t Remaining time (to charge or discharge) in the form `h:min'"
+  :type 'string
+  :group 'battery-usage)
+
+(defun battery-usage-start ()
+  "Start displaying disk usage stats in the mode-line."
+  (interactive)
+
+  (setq battery-mode-line-format battery-usage-battery-format)
+  (display-battery-mode)
+  (setq global-mode-string (delq 'battery-mode-line-string
+                                 global-mode-string))
+  (when battery-usage-use-global-mode-string
+    (add-to-list 'global-mode-string 'battery-usage-mode-line-string t))
+
+  (and battery-usage-timer (cancel-timer battery-usage-timer))
+  (setq battery-usage-mode-line-string "")
+  (setq battery-usage-timer (run-at-time battery-usage-update-interval
+                                        battery-usage-update-interval
+                                        (lambda ()
+                                          (setq battery-usage-mode-line-string (battery-usage))
+                                          (force-mode-line-update)
+                                          (sit-for 0)))))
+
+(defun battery-usage-stop ()
+  "Stop displaying disk usage stats in the mode-line."
+  (interactive)
+  (setq battery-usage-mode-line-string "")
+  (when battery-usage-use-global-mode-string
+    (setq global-mode-string (delq 'battery-usage-mode-line-string
+                                   global-mode-string)))
+  (setq battery-usage-timer
+        (and battery-usage-timer (cancel-timer battery-usage-timer)))
+  (display-battery-mode -1))
+
+(defun battery-usage ()
+  ""
+  (format-battery-usage battery-usage-format))
+
+(defun format-battery-usage (format)
+  ""
+  (let ((stats (battery-stats)))
+    (format-expand battery-usage-formatters format stats)))
+
+(defun battery-stats ()
+  "Return a bunch of disk stats in a form of an alist."
+  (let ((stats (mapcar #'split-string
+                 (remove-if (lambda (str) (string= str ""))
+                            (split-string (substring-no-properties battery-mode-line-string))))))
+    (mapcar (lambda (lst)
+              (cons (car lst)
+                    (mapcar #'string-to-number (cdr lst))))
+            stats)))
+
+(setq battery-usage-formatters
+  (list
+    (cons "c" (lambda (stats)
+                (car (nth 0 stats))))
+    (cons "r" (lambda (stats)
+                (car (nth 1 stats))))
+    (cons "B" (lambda (stats)
+                (car (nth 3 stats))))
+    (cons "d" (lambda (stats)
+                (car (nth 4 stats))))
+    (cons "L" (lambda (stats)
+                (car (nth 5 stats))))
+    (cons "p" (lambda (stats)
+                (car (nth 6 stats))))
+    (cons "m" (lambda (stats)
+                (car (nth 7 stats))))
+    (cons "h" (lambda (stats)
+                (car (nth 8 stats))))
+    (cons "t" (lambda (stats)
+                (car (nth 9 stats))))))
+
+(provide 'battery-stats)
+;;; battery-stats.el ends here
